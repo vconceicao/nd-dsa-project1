902b83e69a8196e2663fc838e8e411170c952140
Vinicius <vinc-analist@hotmail.com>
2020-08-30 22:15:31 -0300

    added Task0 and Task1 and time complexity

--- Task1.py
+++ Task1.py
@@ -18,3 +18,22 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def get_different_phone_numbers(list):
+    diff_list = set()
+    first_column = 0
+    second_column = 1
+    i = 0
+    while len(list)>i:
+        diff_list.add(list[i][first_column])
+        diff_list.add(list[i][second_column])
+        i+=1
+    return diff_list
+
+merged_list = texts + calls
+diff_list = get_different_phone_numbers(merged_list)
+diff_phone_numbers_count = len(diff_list)
+print("There are "+ str(diff_phone_numbers_count) +" different telephone numbers in the records")
+
+
+
---
+++ Test1.py
@@ -0,0 +1,13 @@
+import Task1 as t;
+
+
+
+def tests():
+
+    assert t.diff_phone_numbers_count == 570;
+    
+
+    print('All tests passed')
+
+
+tests();
